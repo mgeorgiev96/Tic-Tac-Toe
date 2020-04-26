eb4f79eb8e27e608b7984a6fed459cb3a34ec810
Martin Georgiev <mgeorgiev9611@abv.bg>
2020-04-26 15:45:26 +0100

    Updates

---
+++ precache-manifest.78fbef902af26c8b0264dfb457f9f866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26cc3132101a0968a47c7e12c1f3e9c1",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "f25e91a725c9553b6dd8",
+    "url": "/Tic-Tac-Toe/static/css/main.556c29d8.chunk.css"
+  },
+  {
+    "revision": "a65b31fbdb61d7c7a839",
+    "url": "/Tic-Tac-Toe/static/js/2.bd1d0d55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.bd1d0d55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f25e91a725c9553b6dd8",
+    "url": "/Tic-Tac-Toe/static/js/main.f855cb79.chunk.js"
+  },
+  {
+    "revision": "81f931a37485fd8540bc",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.55a4db76.js"
+  }
+]);
